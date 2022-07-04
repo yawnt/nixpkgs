6a5601ffa2bab95e3b5138c6863401c803c471db
ajs124 <git@ajs124.de>
2022-07-04 02:51:02 +0200

    hurl: pin to openssl 1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7312,7 +7312,9 @@ with pkgs;
 
   httpx = callPackage ../tools/security/httpx { };
 
-  hurl = callPackage ../tools/networking/hurl { };
+  hurl = callPackage ../tools/networking/hurl {
+    openssl = openssl_1_1;
+  };
 
   hub = callPackage ../applications/version-management/git-and-tools/hub { };
 
