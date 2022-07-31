f05eebda03d76020ec4ba39b2bee0ade29c736fd
ajs124 <git@ajs124.de>
2022-07-31 23:57:36 +0200

    git-backup: pin to openssl_1_1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6537,6 +6537,7 @@ with pkgs;
   git-appraise = callPackage ../applications/version-management/git-and-tools/git-appraise {};
 
   git-backup = callPackage ../applications/version-management/git-backup {
+    openssl = openssl_1_1;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
